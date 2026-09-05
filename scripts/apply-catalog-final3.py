86c75db0a71b1153f0eaf5ece15a2999f9ded610
aamrabdelhay <aamrabdelhay@gmail.com>
2026-09-05 16:55:15 +0300

    Remove obsolete patch scripts

--- scripts/apply-catalog-final3.py
+++
@@ -1,4 +0,0 @@
-from pathlib import Path
-code=Path('scripts/apply-catalog-final2.py').read_text()
-code=code.replace(r'\n?function',r'\n+\s*function')
-exec(compile(code,'scripts/apply-catalog-final2.py','exec'))
